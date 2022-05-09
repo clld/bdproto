c295923a76d94a223c24637cbe29d0b14ecb8b17
Tarik Havighorst <code@blurks.de>
2022-05-09 13:39:59 +0200

    added Home menu item

--- bdproto/__init__.py
+++ bdproto/__init__.py
@@ -6,26 +6,27 @@ from pyramid.config import Configurator
 from clld.interfaces import IMapMarker, IValueSet, IValue, IDomainElement
 from clldutils.svg import pie, icon, data_url
 from clld.web.app import menu_item
+
 # we must make sure custom models are known at database initialization!
 from bdproto import models
 
 
-
-
-
 def main(global_config, **settings):
-    """ This function returns a Pyramid WSGI application.
-    """
-    settings['route_patterns'] = {
-        'contributions': '/inventories',
-        'contribution': r'/inventories/view/{id:[^/\.]+}',
+    """This function returns a Pyramid WSGI application."""
+    settings["route_patterns"] = {
+        "contributions": "/inventories",
+        "contribution": r"/inventories/view/{id:[^/\.]+}",
     }
     config = Configurator(settings=settings)
-    config.include('clld.web.app')
+    config.include("clld.web.app")
 
-    config.include('clldmpg')
+    config.include("clldmpg")
 
     config.register_menu(
+        (
+            "dataset",
+            functools.partial(menu_item, "dataset", label="Home"),
+        ),
         (
             "languages",
             functools.partial(menu_item, "languages", label="Languages"),
