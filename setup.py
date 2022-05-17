30214281a54fed9f5cc0c077e5c143e577e9914c
Tarik Havighorst <code@blurks.de>
2022-05-17 14:21:53 +0200

    added psycopg2 dependency

--- setup.py
+++ setup.py
@@ -2,44 +2,43 @@ from setuptools import setup, find_packages
 
 
 setup(
-    name='bdproto',
-    version='0.0',
-    description='bdproto',
+    name="bdproto",
+    version="0.0",
+    description="bdproto",
     classifiers=[
         "Programming Language :: Python",
         "Framework :: Pyramid",
         "Topic :: Internet :: WWW/HTTP",
         "Topic :: Internet :: WWW/HTTP :: WSGI :: Application",
     ],
-    author='',
-    author_email='',
-    url='',
-    keywords='web pyramid pylons',
+    author="",
+    author_email="",
+    url="",
+    keywords="web pyramid pylons",
     packages=find_packages(),
     include_package_data=True,
     zip_safe=False,
     install_requires=[
-        'clld',  # >=7.0
-
-
-        'clldmpg',
-
-],
-extras_require={
-        'dev': ['flake8', 'waitress'],
-        'test': [
-            'mock',
-            'pytest>=5.4',
-            'pytest-clld',
-            'pytest-mock',
-            'pytest-cov',
-            'coverage>=4.2',
-            'selenium',
-            'zope.component>=3.11.0',
+        "clld",  # >=7.0
+        "clldmpg",
+        "psycopg2",
+    ],
+    extras_require={
+        "dev": ["flake8", "waitress"],
+        "test": [
+            "mock",
+            "pytest>=5.4",
+            "pytest-clld",
+            "pytest-mock",
+            "pytest-cov",
+            "coverage>=4.2",
+            "selenium",
+            "zope.component>=3.11.0",
         ],
     },
     test_suite="bdproto",
     entry_points="""\
     [paste.app_factory]
     main = bdproto:main
-""")
+""",
+)
